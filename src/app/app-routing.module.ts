24825cde1091fac76df518ff2e11a6f8fa2025ac
Andrew-LeGrand <111323440+Andrew-LeGrand@users.noreply.github.com>
2022-10-11 18:40:08 -0500

    Class 15 Routing Exercise

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { AboutComponent } from './about/about.component';
+import { BobComponent } from './about/bob/bob.component';
+import { SusanComponent } from './about/susan/susan.component';
+import { ContactComponent } from './contact/contact.component';
+import { HomeComponent } from './home/home.component';
+
+const routes: Routes = [
+  {path: '', redirectTo: '/home', pathMatch: 'full'},
+  {path: 'home', component: HomeComponent },
+  {path: 'about', component: AboutComponent, children:[
+    { path: 'bob', component: BobComponent},
+    { path: 'susan', component: SusanComponent}]
+  },
+  {path: 'contact', component: ContactComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
